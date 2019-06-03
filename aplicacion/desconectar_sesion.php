4a2ef12ed93a7b3e093e89279ca19e1dbf368b52
PaulLora <PaulLora@users.noreply.github.com>
2019-06-02 23:01:24 -0500

    Login
    La redirección cuando se cierra sesión estaba mal

--- aplicacion/desconectar_sesion.php
+++ aplicacion/desconectar_sesion.php
@@ -3,9 +3,9 @@ session_start();
 if($_SESSION['usuario']){
         session_unset();
 	session_destroy();
-	echo "<script>location.href='formularios_registro/Login.php'</script>";
+	echo "<script>location.href='formularios_registro/Login.html'</script>";
 }
 else{
-	echo "<script>location.href='formularios_registro/Login.php'</script>";
+	echo "<script>location.href='formularios_registro/Login.html'</script>";
 }
 ?>
