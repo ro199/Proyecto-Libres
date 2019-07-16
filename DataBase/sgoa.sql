51ab83b0a8d9fcf5dd118dd7a41470d911cf11c3
ro199 <49215567+ro199@users.noreply.github.com>
2019-07-16 17:43:10 -0500

    base

--- DataBase/sgoa.sql
+++ DataBase/sgoa.sql
@@ -1,13 +1,11 @@
-﻿u-- phpMyAdmin SQL Dump
--- version 4.8.0.1
 -- phpMyAdmin SQL Dump
--- version 4.8.0.1
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-08-2018 a las 04:19:41
--- Versión del servidor: 10.1.32-MariaDB
--- Versión de PHP: 7.2.5
+-- Tiempo de generación: 17-07-2019 a las 00:10:07
+-- Versión del servidor: 10.1.38-MariaDB
+-- Versión de PHP: 7.1.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -123,18 +121,33 @@ CREATE TABLE `estudiante` (
   `id_usuario` int(11) DEFAULT NULL,
   `fecha_nacimiento` varchar(45) DEFAULT NULL,
   `genero` varchar(1) DEFAULT NULL,
-   `ciudad` varchar(10) DEFAULT NULL,
+  `ciudad` varchar(10) DEFAULT NULL,
   `domicilio` varchar(60) DEFAULT NULL,
   `celular` varchar(15) DEFAULT NULL,
   `convencional` varchar(15) DEFAULT NULL
- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `estudiante`
 --
 
-INSERT INTO `estudiante` (`idestudiante`, `ci`, `nombres`, `apellidos`, `carrera`, `id_facultad`, `mail`, `id_usuario`) VALUES
-(1, '1714296603', 'Karina', 'Tapia', 'Sistemas', 8, 'alexisalex31@hotmail.com', 2);
+INSERT INTO `estudiante` (`idestudiante`, `ci`, `nombres`, `apellidos`, `carrera`, `id_facultad`, `mail`, `id_usuario`, `fecha_nacimiento`, `genero`, `ciudad`, `domicilio`, `celular`, `convencional`) VALUES
+(1, '1714296603', 'Karina', 'Tapia', 'Sistemas', 8, 'alexisalex31@hotmail.com', 2, NULL, NULL, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `experiencia`
+--
+
+CREATE TABLE `experiencia` (
+  `idComentario` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `descripcion` varchar(60) DEFAULT NULL,
+  `comentario` varchar(200) DEFAULT NULL,
+  `respondido` varchar(2) DEFAULT 'NO',
+  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -166,6 +179,32 @@ INSERT INTO `facultad` (`idfacultad`, `facultad`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `foro`
+--
+
+CREATE TABLE `foro` (
+  `idForo` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `titulo` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `mensaje` text COLLATE utf8_unicode_ci NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `respuestas` int(11) NOT NULL DEFAULT '0',
+  `identificador` int(7) NOT NULL DEFAULT '0',
+  `ult_respuesta` date DEFAULT NULL,
+  `imagen` varchar(45) COLLATE utf8_unicode_ci DEFAULT '',
+  `video` varchar(45) COLLATE utf8_unicode_ci DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `foro`
+--
+
+INSERT INTO `foro` (`idForo`, `idUsuario`, `titulo`, `mensaje`, `fecha`, `respuestas`, `identificador`, `ult_respuesta`, `imagen`, `video`) VALUES
+(3, 1, 'presentacion', 'presentar proyecto', '2019-04-24 03:39:46', 1, 0, NULL, 'imagenes/WhatsApp Image 2019-02-23 at 20.01.0', 'videos/WhatsApp Image 2019-02-23 at 20.01.00.');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `materia`
 --
 
@@ -185,16 +224,16 @@ INSERT INTO `materia` (`materia_id`, `materia_nombre`, `facultad_id`, `cantidad`
 (2, 'FUNDAMENTOS DE CIENCIAS DE COMPUTACIÓN', 8, 0),
 (3, 'PROGRAMACIÓN I ', 8, 0),
 (4, 'MATEMÁTICAS DISCRETAS', 8, 0),
-(5, 'PROGRAMACIÓN II ', 8, 0),
+(5, 'PROGRAMACIÓN II ', 8, 1),
 (6, 'SISTEMAS DE COMUNICACIÓN ', 8, 0),
 (7, 'ARQUITECTURA DE COMPUTADORES ', 8, 0),
-(8, 'ESTRUCTURA DE DATOS ', 8, 0),
+(8, 'ESTRUCTURA DE DATOS ', 8, 1),
 (9, 'REDES DE COMPUTADORES ', 8, 0),
 (10, 'SISTEMAS OPERATIVOS ', 8, 0),
 (11, 'COMPILADORES Y LENGUAJES', 8, 0),
-(12, 'BASES DE DATOS', 8, 0),
-(13, 'ALGORITMOS', 8, 3),
-(14, 'ALGORITMOS NUMERICOS', 8, 0),
+(12, 'BASES DE DATOS', 8, 1),
+(13, 'ALGORITMOS', 8, 5),
+(14, 'ALGORITMOS NUMERICOS', 8, 3),
 (15, 'GESTION DE PROYECTOS ', 8, 0),
 (16, 'TCP-IP', 8, 0),
 (17, 'TECNOLOGÍAS DE SEGURIDAD', 8, 0),
@@ -205,7 +244,7 @@ INSERT INTO `materia` (`materia_id`, `materia_nombre`, `facultad_id`, `cantidad`
 (22, 'ADMINISTRACIÓN FINANCIERA', 8, 0),
 (23, 'COMPUTACIÓN DISTRIBUIDA', 8, 0),
 (24, 'INTELIGENCIA DE NEGOCIOS', 8, 0),
-(25, 'INGENIERÍA DE SOFTWARE II', 8, 1),
+(25, 'INGENIERÍA DE SOFTWARE II', 8, 4),
 (26, 'APLICACIONES EN AMBIENTES LIBRES', 8, 0),
 (27, 'LEGISLACIÓN INFORMÁTICA', 8, 0),
 (28, 'DISEÑO DE PROCESOS ORGANIZACIONALES', 8, 0),
@@ -248,7 +287,7 @@ INSERT INTO `materia` (`materia_id`, `materia_nombre`, `facultad_id`, `cantidad`
 (65, 'MAQUINAS ELECTRICAS', 4, 0),
 (66, 'GEOESTADISTICA', 5, 0),
 (67, 'GEOTECNIA', 5, 0),
-(68, 'GEOQUIMICA', 5, 1),
+(68, 'GEOQUIMICA', 5, 2),
 (69, 'MAPAS GEOLOGICOS', 5, 0),
 (70, 'PETROLOGIA SEDUMENTARIA', 5, 0),
 (71, 'TECTONICA DE PLACAS', 5, 0),
@@ -289,13 +328,29 @@ CREATE TABLE `objeto_aprendizaje` (
   `ruta` varchar(45) DEFAULT NULL,
   `materia` varchar(50) DEFAULT NULL,
   `tipo_repo` int(2) DEFAULT NULL,
-  `descarga` int(11) DEFAULT NULL
+  `descarga` int(11) DEFAULT NULL,
+  `Privado` varchar(3) DEFAULT NULL,
+  `Publico` varchar(3) DEFAULT NULL,
+  `direcurl` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `objeto_aprendizaje`
 --
 
+INSERT INTO `objeto_aprendizaje` (`idobjeto_aprendizaje`, `nombre`, `descripcion`, `id_usuario`, `institucion`, `fechaCreacion`, `palabras_clave`, `tamanio`, `ruta`, `materia`, `tipo_repo`, `descarga`, `Privado`, `Publico`, `direcurl`) VALUES
+(2, 'github', 'Ayuda', 4, 'EPN', '2019-06-30 18:23:26', 'git', '813', '../../storage/github.txt', 'ALGORITMOS', 1, 0, 'htt', 'SI', NULL),
+(3, 'Patrones', 'Inteligencia de negocios', 4, 'EPN', '2019-06-30 18:35:49', 'Metodos', '1583', '../../storage/Patrones.txt', 'ALGORITMOS NUMERICOS', 1, 0, 'SI', 'SI', NULL),
+(4, 'LOLO', 'LO', 4, 'EPN', '2019-07-01 00:30:09', 'LO', '77807', '../../storage/LOLO.jpeg', 'ESTRUCTURA DE DATOS ', 1, 0, 'SI', NULL, NULL),
+(5, 'uu', 'unasd', 4, 'EPN', '2019-07-01 00:37:58', 'ASDASD', '66179', '../../storage/uu.jpeg', 'GEOQUIMICA', 1, 0, 'SI', NULL, NULL),
+(6, 'Software', 'Met', 4, 'EPN', '2019-07-01 03:15:11', 'xp', '2133', '../../storage/Software.txt', 'INGENIERÍA DE SOFTWARE II', 1, 0, 'SI', NULL, NULL),
+(7, 'asdweasdsadasdasdasdasd', 'asdasdasdasdasdsa', 4, 'EPN', '2019-07-01 03:36:32', 'ew', '1339', '../../storage/asdweasdsadasdasdasdasd.txt', 'ALGORITMOS NUMERICOS', 1, 0, 'SI', NULL, NULL),
+(8, 'XP', 'xp', 4, 'EPN', '2019-07-01 03:45:53', 'metodologia', '1270', '../../storage/XP.txt', 'INGENIERÍA DE SOFTWARE II', 1, 0, 'SI', NULL, NULL),
+(9, 'Base de datos', 'Libres', 4, 'EPN', '2019-07-01 21:53:19', 'Server', '30098', '../../storage/Base+de+datos.sql', 'PROGRAMACIÓN II ', 1, 0, 'SI', NULL, NULL),
+(10, 'Libres1', 'Parte del codigo', 2, 'EPN', '2019-07-01 23:26:14', 'Li', '13207', '../../storage/Libres1.php', 'BASES DE DATOS', 1, 0, 'SI', 'SI', NULL),
+(11, 'Test Driven', 'metodologia', 4, 'EPN', '2019-07-02 02:33:49', 'metodologia', '336', '../../storage/Test+Driven.txt', 'INGENIERÍA DE SOFTWARE II', 1, 0, 'SI', 'SI', NULL),
+(12, 'Wifi', 'metodologia', 4, 'EPN', '2019-07-02 19:54:58', 'wIFI', '34', '../../storage/Wifi.txt', 'ALGORITMOS NUMERICOS', 1, 0, 'SI', 'SI', NULL),
+(13, 'Base', 'Base', 4, 'EPN', '2019-07-02 23:52:56', 'Base', '20804', '../../storage/Base.sql', NULL, 1, 0, 'SI', 'NO', NULL);
 
 -- --------------------------------------------------------
 
@@ -324,10 +379,10 @@ CREATE TABLE `profesor` (
 -- Volcado de datos para la tabla `profesor`
 --
 
-INSERT INTO `profesor` (`idprofesor`, `ci`, `nombres`, `apellidos`, `id_departamento`, `id_facultad`, `mail`, `id_usuario`) VALUES
-(1, '1725790032', 'Alexis', 'Maldonado', 11, 2, 'alexis.maldonado@epn.edu.ec', 4),
-(2, '1725790032', 'Fernando', 'Tapia', 7, 6, 'alexisalex31@hotmail.com', 5),
-(3, '0802548578', 'Maria', 'Intriago', 17, 8, 'monserrate.intriago@epn.edu.ec', 9);
+INSERT INTO `profesor` (`idprofesor`, `ci`, `nombres`, `apellidos`, `id_departamento`, `id_facultad`, `mail`, `id_usuario`, `fecha_nacimiento`, `genero`, `ciudad`, `domicilio`, `celular`, `convencional`) VALUES
+(1, '1725790032', 'Alexis', 'Maldonado', 11, 2, 'alexis.maldonado@epn.edu.ec', 4, NULL, NULL, NULL, NULL, NULL, NULL),
+(2, '1725790032', 'Fernando', 'Tapia', 7, 6, 'alexisalex31@hotmail.com', 5, NULL, NULL, NULL, NULL, NULL, NULL),
+(3, '0802548578', 'Maria', 'Intriago', 17, 8, 'monserrate.intriago@epn.edu.ec', 9, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -349,15 +404,18 @@ CREATE TABLE `usuario` (
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`idUsuario`, `usuario`, `contrasenia`, `tipo_usuario`, `activo`,`cambio_de_clave`) VALUES
-(1, 'PaulLora1', '123', 'EST', 'V', 'F'),
-(2, 'ktapia', 'hola123', 'EST', 'V', 'F'),
-(4, 'AlexisMaldonado0', 'alexis', 'PRO', 'V', 'F'),
-(5, 'FernandoTapia0', 'bESn9', 'PRO', 'F', 'F'),
-(6, 'mmjaramilloarteaga', 'mmjaramilloarteaga', 'EST', 'V', 'F'),
-(7, 'mmjaramillo', 'mmjaramillo', 'EST', 'V', 'F'),
-(8, 'mmjaramillo', '123', 'EST', 'V', 'F'),
-(9, 'MariaIntriago0', 'gcjFI', 'PRO', 'V', 'F');
+INSERT INTO `usuario` (`idUsuario`, `usuario`, `contrasenia`, `tipo_usuario`, `activo`, `cambio_de_clave`, `foto`) VALUES
+(1, 'PaulLora1', '123', 'ADM', 'V', 'F', ''),
+(2, 'ktapia', 'hola123', 'EST', 'V', 'F', ''),
+(4, 'AlexisMaldonado0', 'alexis', 'PRO', 'V', 'F', ''),
+(5, 'FernandoTapia0', 'bESn9', 'PRO', 'F', 'F', ''),
+(6, 'mmjaramilloarteaga', 'mmjaramilloarteaga', 'EST', 'V', 'F', ''),
+(7, 'mmjaramillo', 'mmjaramillo', 'EST', 'V', 'F', ''),
+(8, 'mmjaramillo', '123', 'EST', 'V', 'F', ''),
+(9, 'MariaIntriago0', 'gcjFI', 'PRO', 'V', 'F', ''),
+(10, 'AngelYanez0', 'aCOn9', 'EST', 'V', 'T', ''),
+(11, 'CarolOña0', 'G2aOI', 'EST', 'V', 'T', ''),
+(12, 'PaulLora0', '1234', 'PRO', 'V', 'F', '');
 
 -- --------------------------------------------------------
 
@@ -382,7 +440,12 @@ INSERT INTO `valoracion` (`idvaloracion`, `idobjeto_aprendizaje`, `idusuario`, `
 (4, 12, 2, 4),
 (5, 12, 2, 4),
 (6, 12, 2, 1),
-(7, 12, 4, 1);
+(7, 12, 4, 1),
+(8, 3, 4, 3);
+
+--
+-- Índices para tablas volcadas
+--
 
 --
 -- Indices de la tabla `catalogooa`
@@ -413,10 +476,24 @@ ALTER TABLE `estudiante`
   ADD KEY `fk_estudiante_facultad1_idx` (`id_facultad`);
 
 --
+-- Indices de la tabla `experiencia`
+--
+ALTER TABLE `experiencia`
+  ADD PRIMARY KEY (`idComentario`),
+  ADD KEY `idUsuario` (`idUsuario`);
+
+--
 -- Indices de la tabla `facultad`
 --
 ALTER TABLE `facultad`
   ADD PRIMARY KEY (`idfacultad`);
+
+--
+-- Indices de la tabla `foro`
+--
+ALTER TABLE `foro`
+  ADD PRIMARY KEY (`idForo`),
+  ADD KEY `idUsuario` (`idUsuario`);
 
 --
 -- Indices de la tabla `materia`
@@ -482,10 +559,22 @@ ALTER TABLE `estudiante`
   MODIFY `idestudiante` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT de la tabla `experiencia`
+--
+ALTER TABLE `experiencia`
+  MODIFY `idComentario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `facultad`
 --
 ALTER TABLE `facultad`
   MODIFY `idfacultad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `foro`
+--
+ALTER TABLE `foro`
+  MODIFY `idForo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `materia`
@@ -497,7 +586,7 @@ ALTER TABLE `materia`
 -- AUTO_INCREMENT de la tabla `objeto_aprendizaje`
 --
 ALTER TABLE `objeto_aprendizaje`
-  MODIFY `idobjeto_aprendizaje` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `idobjeto_aprendizaje` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `profesor`
@@ -509,24 +598,17 @@ ALTER TABLE `profesor`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `valoracion`
 --
 ALTER TABLE `valoracion`
-  MODIFY `idvaloracion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `idvaloracion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Restricciones para tablas volcadas
 --
-
---
--- Filtros para la tabla `comentario`
---
-ALTER TABLE `comentario`
-  ADD CONSTRAINT `fk_comentario_objeto_aprendizaje1` FOREIGN KEY (`id_objeto_aprendizaje`) REFERENCES `objeto_aprendizaje` (`idobjeto_aprendizaje`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_comentario_usuario1` FOREIGN KEY (`idusuario`) REFERENCES `usuario` (`idUsuario`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `estudiante`
@@ -534,6 +616,18 @@ ALTER TABLE `comentario`
 ALTER TABLE `estudiante`
   ADD CONSTRAINT `fk_estudiante_facultad1` FOREIGN KEY (`id_facultad`) REFERENCES `facultad` (`idfacultad`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_estudiante_usuario1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`idUsuario`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `experiencia`
+--
+ALTER TABLE `experiencia`
+  ADD CONSTRAINT `experiencia_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`);
+
+--
+-- Filtros para la tabla `foro`
+--
+ALTER TABLE `foro`
+  ADD CONSTRAINT `foro_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`);
 
 --
 -- Filtros para la tabla `materia`
@@ -546,60 +640,6 @@ ALTER TABLE `materia`
 --
 ALTER TABLE `objeto_aprendizaje`
   ADD CONSTRAINT `fk_objeto_aprendizaje_usuario1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`idUsuario`) ON DELETE CASCADE ON UPDATE NO ACTION;
-
---
--- Filtros para la tabla `profesor`
---
-ALTER TABLE `profesor`
-  ADD CONSTRAINT `fk_profesor_departamento1` FOREIGN KEY (`id_departamento`) REFERENCES `departamento` (`iddepartamento`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_profesor_facultad1` FOREIGN KEY (`id_facultad`) REFERENCES `facultad` (`idfacultad`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_profesor_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`idUsuario`) ON DELETE CASCADE ON UPDATE NO ACTION;
-COMMIT;
-
-CREATE TABLE `experiencia` (
- `idComentario` INT AUTO_INCREMENT PRIMARY KEY,
- `idUsuario` INT NOT NULL,
- `descripcion` VARCHAR(60),
- `comentario` VARCHAR(200),
- `respondido` VARCHAR(2) DEFAULT 'NO',
-  `fecha` timestamp DEFAULT CURRENT_TIMESTAMP);
-
-  ALTER TABLE `experiencia`
-  ADD FOREIGN KEY (idUsuario)
-  REFERENCES usuario(idUsuario);
-
-CREATE TABLE IF NOT EXISTS `foro` (
-  `idForo` INT AUTO_INCREMENT PRIMARY KEY,
-  `idUsuario` INT NOT NULL,
-  `titulo` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
-  `mensaje` text COLLATE utf8_unicode_ci NOT NULL,
-  `fecha` timestamp DEFAULT CURRENT_TIMESTAMP,
-  `respuestas` int(11) NOT NULL DEFAULT '0',
-  `identificador` int(7) NOT NULL DEFAULT '0',
-  `ult_respuesta` date DEFAULT NULL, 
-  `imagen` varchar(45) DEFAULT '',
-  `video` varchar(45) DEFAULT ''
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-ALTER TABLE `foro`
-  ADD FOREIGN KEY (idUsuario)
-  REFERENCES usuario(idUsuario);
-
---
--- Estructura de tabla para la tabla `colaborador`
---
-
-CREATE TABLE IF NOT EXISTS `colaborador` (
-  `idColaborador` int(11) AUTO_INCREMENT PRIMARY KEY,
-  `idUsuario` int(11) NOT NULL,
-  `activo` varchar(1) DEFAULT 'T',
-  `colaboraciones` int(11) DEFAULT 0 
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-ALTER TABLE `colaborador`
-  ADD FOREIGN KEY (idUsuario)
-  REFERENCES usuario(idUsuario);
-
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
